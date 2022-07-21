951e0fa2668a99fa88d1a8d3f3eeec1442a8cb62
jong lee <lsljw94@daum.net>
2022-07-21 16:38:09 +0900

    모바일 메뉴 처리

--- js/common.js
+++ js/common.js
@@ -5,4 +5,27 @@ $(document).ready(function(){
     $(".modal-close").click(function(){
         $(".modal-wrap").hide();
     });
+
+    /* $(".m-open").click(function(){
+        $(".m-content").slideDown();
+    });
+    $(".m-close").click(function(){
+        $(".m-content").slideUp();
+    }) */
+    /* $(".m-open").click(function(){
+        $(".m-content").show();
+    });
+    $(".m-close").click(function(){
+        $(".m-content").hide();
+    }) */
+    $(".m-open").click(function(){
+        $(".m-content").css({
+            "left":0
+        });
+    });
+    $(".m-close").click(function(){
+        $(".m-content").css({
+            "left":"-91%"
+        })
+    })
 });
